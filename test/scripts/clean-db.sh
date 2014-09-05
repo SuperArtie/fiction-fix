1d1cbaa4aa5bba563bbe90733571e3ad4b895e5a
DSRoden <Daniel.S.Roden@gmail.com>
2014-09-05 11:35:46 -0500

    json

--- test/scripts/clean-db.sh
+++ test/scripts/clean-db.sh
@@ -5,6 +5,10 @@ if [ -z "$1" ] ; then
   exit 1
 fi
 
-mongoimport --jsonArray --drop --db $1 --collection examples --file ../../db/example.json
 mongoimport --jsonArray --drop --db $1 --collection users --file ../../db/users.json
+mongoimport --jsonArray --drop --db $1 --collection items --file ../../db/items.json
+mongoimport --jsonArray --drop --db $1 --collection gifts --file ../../db/gifts.json
+mongoimport --jsonArray --drop --db $1 --collection messages --file ../../db/messages.json
+mongoimport --jsonArray --drop --db $1 --collection winks --file ../../db/winks.json
+mongoimport --jsonArray --drop --db $1 --collection proposals --file ../../db/proposals.json
 
