61f206e45c282d965cb6deaa730163a6d4043b7c
Nathan Vasquez <nathan.superartie@gmail.com>
2014-09-07 17:17:47 -0500

    fixed authenticate methods, dashboards now fully operational, also waiting to merge with the search functions, still need to optimize the messages inbox, also need to merge juan branch.

--- config.js
+++ config.js
@@ -10,19 +10,19 @@ config.stripe = {
 config.twitter = {
   apiKey      : 'LNfG2qlieNXHUEPrXx8bcEeiq',
   apiSec      : process.env.TWITTER_SECRET,
-  callbackURL : 'http://mikey-vm.com:3333/auth/twitter/callback'
+  callbackURL : 'http://deluxin.com:3000/auth/twitter/callback'
 };
 
 config.facebook = {
-  clientId    : '344026865773316',
+  clientId    : '338714129622471',
   clientSecret: process.env.FACEBOOK_SECRET,
-  callbackURL : 'http://mikey-vm.com:3333/auth/facebook/callback'
+  callbackURL : 'http://deluxin.com:3000/auth/facebook/callback'
 };
 
 config.google = {
   clientId    : '75269371673-d5pes8pmg7t39s5q4v88rjaulne52ltg.apps.googleusercontent.com',
   clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL : 'http://mikey-vm.com:3333/auth/google/callback'
+  callbackURL : 'http://deluxin.com:3000/auth/google/callback'
 };
 
 module.exports = config;
