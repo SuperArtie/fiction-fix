c09ccdf72938ead790e22b2b7479268bfe8473ff
DSRoden <Daniel.S.Roden@gmail.com>
2014-09-07 21:54:00 -0500

    revised and enhanced proposals

--- app/models/message.js
+++ app/models/message.js
@@ -8,6 +8,7 @@ function Message(senderId, receiverId, message){
   this.receiverId = receiverId;
   this.body       = message;
   this.date       = new Date();
+  this.isRead     = false;
 }
 
 Object.defineProperty(Message, 'collection', {
@@ -28,10 +29,10 @@ Message.all = function(cb){
   Message.collection.find().toArray(cb);
 };
 
-Message.messages = function(receiverId, cb){
-  var _id = Mongo.ObjectID(receiverId);
-  Message.collection.find({receiverId:_id}).toArray(function(err, messages){
-    async.map(messages, iterator, cb);
+Message.read = function(msgId, cb){
+  var _id= Mongo.ObjectID(msgId);
+  Message.collection.findAndModify({_id:_id}, [], {$set:{isRead:true}}, function(err, msg){
+    iterator(msg, cb);
   });
 };
 
@@ -39,6 +40,18 @@ Message.send = function(senderId, receiverId, message, cb){
   var m = new Message(senderId, receiverId, message);
   Message.collection.save(m, cb);
 };
+
+Message.unread = function(receiverId, cb){
+  Message.collection.find({receiverId:receiverId, isRead:false}).count(cb);
+};
+
+Message.messages = function(receiverId, cb){
+  var _id = Mongo.ObjectID(receiverId);
+  Message.collection.find({receiverId:_id}).sort({date:-1}).toArray(function(err, messages){
+    async.map(messages, iterator, cb);
+  });
+};
+
 
 module.exports = Message;
 
